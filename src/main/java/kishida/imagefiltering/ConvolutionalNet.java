900fad33c79c253924d6ff915687be7dfa9bc2e4
naoki <naoki@anna>
2015-08-03 23:20:46 +0900

    畳み込みネット、前向き処理を記述

--- src/main/java/kishida/imagefiltering/ConvolutionalNet.java
+++ src/main/java/kishida/imagefiltering/ConvolutionalNet.java
@@ -14,7 +14,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import javax.imageio.ImageIO;
 
 /**
@@ -22,7 +25,6 @@ import javax.imageio.ImageIO;
  * @author naoki
  */
 public class ConvolutionalNet {
-    static final int stride = 4;
     static final double ep = 0.000000001;
 
     static class Img{
@@ -35,163 +37,234 @@ public class ConvolutionalNet {
         boolean inverse;
     }
     
-    public static void main(String[] args) throws IOException {
-        //一段目のフィルタとバイアス
-        //二段目のフィルタとバイアス
-        //全結合1とバイアス
-        //全結合2とバイアス
+    /** 畳み込み層 */
+    static class ConvolutionLayer{
+        double[][][][] filter;
+        double[] bias;
+        int stride;
+
+        public ConvolutionLayer(int filterCount, int channel, int size, int stride) {
+            this.filter = Stream.generate(() -> Stream.generate(() -> createRandomFilter(size))
+                            .limit(channel).toArray(len -> new double[len][][]))
+                        .limit(filterCount).toArray(len -> new double[len][][][]);
+            this.bias = DoubleStream.generate(() -> r.nextDouble()).limit(filterCount).toArray();
+            this.stride = stride;
+        }
+        /** 畳み込みフィルタを適用する */
+        double[][][] applyFilter(double[][][] img) {
+            int width = img[0].length;
+            int height = img[0][0].length;
+            int filterSize = filter[0][0].length;
+            double[][][] result = new double[filter.length][width / stride][height / stride];
+            IntStream.range(0, filter.length).parallel().forEach(fi ->{
+                for(int x = 0; x < width / stride; ++x){
+                    for(int y = 0; y < height / stride; ++y){
+                        for(int ch = 0; ch < filter[fi].length; ++ch){
+                            for(int i = 0; i < filter[0][0].length; ++i){
+                                int xx = x * stride + i - filterSize / 2;
+                                if(xx < 0 || xx >= width){
+                                    continue;
+                                }
+                                for(int j = 0; j < filter[0][0][0].length; ++j){
+                                    int yy = y * stride + j - filterSize / 2;
+                                    if(yy < 0 || yy >= height){
+                                        continue;
+                                    }
+                                    result[fi][x][y] += img[ch][xx][yy] * 
+                                            filter[fi][ch][i][j];
+                                }
+                            }
+                        }
+                        result[fi][x][y] += bias[fi];
+                    }
+                }
+            });
+            return result;
+        }
+
+        /** 畳み込み層の学習 */
+        void learn(double[][][] input, double[][][] delta){
+            IntStream.range(0, filter.length).parallel().forEach(f -> {
+                for(int ch = 0; ch < filter[0].length; ++ch){
+                    for(int x = 0; x < input[0].length / stride; ++x){
+                        for(int y = 0; y < input[0][0].length / stride; ++y){
+                            for(int i = 0; i < filter[0][0].length; ++i){
+                                int xx = x * stride + i - filter[0][0].length / 2;
+                                if(xx < 0 || xx >= input[0].length){
+                                    continue;
+                                }
+                                for(int j = 0; j < filter[0][0][0].length; ++j){
+                                    int yy = y * stride + j - filter[0][0][0].length / 2;
+                                    if(yy < 0 || yy >= input[0][0].length){
+                                        continue;
+                                    }
+                                    double d = (input[ch][xx][yy] > 0 ? 1 : 0) * delta[f][x][y];
+                                    filter[f][ch][i][j] += d * ep;
+                                }
+                            }
+                            bias[f] += ep * delta[f][x][y];
+                        }
+                    }
+                }
+            });
+        }
+    }
+    
+    static class MaxPoolingLayer{
+        int size;
+        int stride;
+
+        public MaxPoolingLayer(int size, int stride) {
+            this.size = size;
+            this.stride = stride;
+        }
+        /** プーリング(max) */
+        double[][][] pooling(double[][][] data){
+            double[][][] result = new double[data.length][data[0].length / stride][data[0][0].length / stride];
+            IntStream.range(0, data.length).parallel().forEach(ch -> {
+                for(int x = 0; x < data[0].length / stride; ++x){
+                    for(int y = 0; y < data[0][0].length / stride; ++y){
+                        double max = Double.NEGATIVE_INFINITY;
+                        for(int i = 0; i < size; ++i){
+                            int xx = x * stride + i - size / 2;
+                            if(xx < 0 || xx >= data[0].length){
+                                continue;
+                            }
+                            for(int j = 0; j < size; ++j){
+                                int yy = y * stride + j - size / 2;
+                                if(yy < 0 || yy >= data[0][0].length){
+                                    continue;
+                                }
+                                if(max < data[ch][xx][yy]){
+                                    max = data[ch][xx][yy];
+                                }
+                            }
+                        }
+                        result[ch][x][y] = max;
+                    }
+                }
+            });
+            return result;
+        }
+
+        double[][][] backwordPooling(double[][][] in, double[][][] delta){
+            double[][][] result = new double[in.length][in[0].length / stride][in[0][0].length / stride];
+            IntStream.range(0, in.length).parallel().forEach(ch -> {
+                for(int x = 0; x < in[0].length / stride; ++x){
+                    for(int y = 0; y < in[0][0].length / stride; ++y){
+                        double max = Double.NEGATIVE_INFINITY;
+                        int maxX = 0;
+                        int maxY = 0;
+                        for(int i = 0; i < size; ++i){
+                            int xx = x * stride + i - size / 2;
+                            if(xx < 0 || xx >= in[0].length){
+                                continue;
+                            }
+                            for(int j = 0; j < size; ++j){
+                                int yy = y * stride + j - size / 2;
+                                if(yy < 0 || yy >= in[0][0].length){
+                                    continue;
+                                }
+                                if(max < in[ch][xx][yy]){
+                                    max = in[ch][xx][yy];
+                                    maxX = xx;
+                                    maxY = yy;
+                                }
+                            }
+                        }
+                        result[ch][x][y] = delta[ch][maxX][maxY];
+                    }
+                }
+            });
+
+
+            return result;
+        }
+    }
+    
+    static class FullyConnect{
+        double[][] weight;
+        double bias;
+        int out;
+        public FullyConnect(int in, int out) {
+            this.out = out;
+            weight = Stream.generate(() -> 
+                    DoubleStream.generate(r::nextDouble).limit(out).toArray()
+            ).limit(in).toArray(len -> new double[len][]);
+            bias = r.nextDouble();
+        }
         
+        public double[] forward(double[] in){
+            double[] result = new double[out];
+            for(int j = 0; j < out; ++j){
+                for(int i = 0; i < in.length; ++i){
+                    result[j] += in[i] * weight[i][j];
+                }
+                result[j] += bias;
+            }
+            
+            return result;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        //一段目
+        ConvolutionLayer conv1 = new ConvolutionLayer(48, 3, 11, 4);
+        //一段目のプーリング
+        MaxPoolingLayer pool1 = new MaxPoolingLayer(3, 2);
+        //二段目
+        ConvolutionLayer conv2 = new ConvolutionLayer(96, 48, 5, 2);
+        //二段目のプーリング
+        MaxPoolingLayer pool2 = new MaxPoolingLayer(3, 2);
+        
+        //全結合1
+        FullyConnect fc1 = new FullyConnect(6144, 32);
+        //全結合2
+        FullyConnect fc2 = new FullyConnect(32, 8);
         
         Path p = Paths.get("C:\\Users\\naoki\\Desktop\\sampleimg\\DSC06399.JPG");
         BufferedImage readImg = ImageIO.read(p.toFile());
-        double[][][] readData = imageToArray(readImg);
+        BufferedImage resized = resize(readImg, 256, 256);
+        double[][][] readData = imageToArray(resized);
         //一段目のフィルタをかける
+        double[][][] filterd1 = conv1.applyFilter(readData);
         //プーリング
+        double[][][] pooled1 = pool1.pooling(filterd1);
         //二段目のフィルタをかける
+        double[][][] filtered2 = conv2.applyFilter(pooled1);
         //プーリング
+        double[][][] pooled2 = pool2.pooling(filtered2);
+        double[] flattenPooled2 = flatten(pooled2);
+        System.out.println(flattenPooled2.length);
         //全結合一段
+        double[] fc1out = fc1.forward(flattenPooled2);
         //全結合二段
-        
+        double[] fc2out = fc2.forward(fc1out);
+        //ソフトマックス
+        double[] output = softMax(fc2out);
+        System.out.println(Arrays.stream(output).mapToObj(d -> String.format("%.3f", d)).collect(Collectors.joining(",")));
         //全結合二段の逆伝播
         //全結合一段の逆伝播
         //プーリングの逆伝播
         //二段目のフィルタの逆伝播
         //プーリングの逆伝播
         //一段目のフィルタの逆伝播
-        
         //一段目のフィルタの表示
         //フィルタ後の表示
         //全結合一段の表示
         //全結合二段の表示
         
     }
-    
-    /** 畳み込みフィルタを適用する */
-    static double[][][] applyFilter(double[][][] img, double[][][][] filter, double[] bias, int inStride) {
-        int width = img[0].length;
-        int height = img[0][0].length;
-        int filterSize = filter[0][0].length;
-        double[][][] result = new double[filter.length][width / inStride][height / inStride];
-        IntStream.range(0, filter.length).parallel().forEach(fi ->{
-            for(int x = 0; x < width / inStride; ++x){
-                for(int y = 0; y < height / inStride; ++y){
-                    for(int ch = 0; ch < filter[fi].length; ++ch){
-                        for(int i = 0; i < filter[0][0].length; ++i){
-                            int xx = x * inStride + i - filterSize / 2;
-                            if(xx < 0 || xx >= width){
-                                continue;
-                            }
-                            for(int j = 0; j < filter[0][0][0].length; ++j){
-                                int yy = y * inStride + j - filterSize / 2;
-                                if(yy < 0 || yy >= height){
-                                    continue;
-                                }
-                                result[fi][x][y] += img[ch][xx][yy] * 
-                                        filter[fi][ch][i][j];
-                            }
-                        }
-                    }
-                    result[fi][x][y] += bias[fi];
-                }
-            }
-        });
-        return result;
-    }
-    
-    /** 畳み込み層の学習 */
-    static void convolutionalLearn(double[][][] delta, double[][][][] filters, double[] bias, double[][][] input, int step ){
-        IntStream.range(0, filters.length).parallel().forEach(f -> {
-            for(int ch = 0; ch < filters[0].length; ++ch){
-                for(int x = 0; x < input[0].length / step; ++x){
-                    for(int y = 0; y < input[0][0].length / step; ++y){
-                        for(int i = 0; i < filters[0][0].length; ++i){
-                            int xx = x * step + i - filters[0][0].length / 2;
-                            if(xx < 0 || xx >= input[0].length){
-                                continue;
-                            }
-                            for(int j = 0; j < filters[0][0][0].length; ++j){
-                                int yy = y * step + j - filters[0][0][0].length / 2;
-                                if(yy < 0 || yy >= input[0][0].length){
-                                    continue;
-                                }
-                                double d = (input[ch][xx][yy] > 0 ? 1 : 0) * delta[f][x][y];
-                                filters[f][ch][i][j] += d * ep;
-                            }
-                        }
-                        bias[f] += ep * delta[f][x][y];
-                    }
-                }
-            }
-        });
-    }
-    
-    /** プーリング層(max) */
-    static double[][][] pooling(double[][][] data, int size){
-        double[][][] result = new double[data.length][data[0].length][data[0][0].length];
-        IntStream.range(0, data.length).parallel().forEach(ch -> {
-            for(int x = 0; x < data[0].length; ++x){
-                for(int y = 0; y < data[0][0].length; ++y){
-                    double max = Double.NEGATIVE_INFINITY;
-                    for(int i = 0; i < size; ++i){
-                        int xx = x + i - size / 2;
-                        if(xx < 0 || xx >= data[0].length){
-                            continue;
-                        }
-                        for(int j = 0; j < size; ++j){
-                            int yy = y + j - size / 2;
-                            if(yy < 0 || yy >= data[0][0].length){
-                                continue;
-                            }
-                            if(max < data[ch][xx][yy]){
-                                max = data[ch][xx][yy];
-                            }
-                        }
-                    }
-                    result[ch][x][y] = max;
-                }
-            }
-        });
-        return result;
+    static void printDim(String name, double[][][] data){
+        System.out.printf("%s:%dx%dx%d%n", name,
+                data[0].length, data[0][0].length, data.length);
     }
 
-    static double[][][] backwordPooling(double[][][] in, double[][][] delta, int size){
-        double[][][] result = new double[in.length][in[0].length][in[0][0].length];
-        IntStream.range(0, in.length).parallel().forEach(ch -> {
-            for(int x = 0; x < in[0].length; ++x){
-                for(int y = 0; y < in[0][0].length; ++y){
-                    double max = Double.NEGATIVE_INFINITY;
-                    int maxX = 0;
-                    int maxY = 0;
-                    for(int i = 0; i < size; ++i){
-                        int xx = x + i - size / 2;
-                        if(xx < 0 || xx >= in[0].length){
-                            continue;
-                        }
-                        for(int j = 0; j < size; ++j){
-                            int yy = y + j - size / 2;
-                            if(yy < 0 || yy >= in[0][0].length){
-                                continue;
-                            }
-                            if(max < in[ch][xx][yy]){
-                                max = in[ch][xx][yy];
-                                maxX = xx;
-                                maxY = yy;
-                            }
-                        }
-                    }
-                    result[ch][x][y] = delta[ch][maxX][maxY];
-                }
-            }
-        });
-        
-        
-        return result;
-    }
-    
     static double[] softMax(double[] output){
         double total = Arrays.stream(output).parallel()
                 .map(d -> Math.exp(d))
                 .sum();
+        System.out.println(total);
         return Arrays.stream(output).parallel()
                 .map(d -> Math.exp(d) / total)
                 .toArray();
@@ -202,12 +275,19 @@ public class ConvolutionalNet {
         double [][] result = new double[size][size];
         for(int i = 0; i < size; ++i){
             for(int j = 0; j < size; ++j){
-                result[i][j] = (r.nextDouble() - 0.5) * 2;
+                result[i][j] = r.nextDouble();
             }
         }
 
         
         return result;
+    }
+    
+    static double[] flatten(double[][][] data){
+        return Arrays.stream(data)
+                .flatMap(Arrays::stream)
+                .flatMapToDouble(Arrays::stream)
+                .toArray();
     }
     
     /** 値のクリッピング */
@@ -222,11 +302,12 @@ public class ConvolutionalNet {
         return resize(imgRead, width, height, false);
     }
     private static BufferedImage resize(BufferedImage imgRead, int width, int height, boolean inverse) {
+        /*
         if(imgRead.getWidth() * height > imgRead.getHeight() * width){
             height = imgRead.getHeight() * width / imgRead.getWidth();
         }else{
             width = imgRead.getWidth() * height / imgRead.getHeight();
-        }
+        }*/
         BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics g = img.getGraphics();
         ((Graphics2D)g).setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
